8bde7473dd3f266a445483fd9d0ff17a0c6be9bf
Michael Jendrusch <jendrusch@stud.uni-heidelberg.de>
2018-01-16 12:33:59 +0100

    added tests for objectiveClass and importClass

--- objc.nimble
+++ objc.nimble
@@ -9,3 +9,25 @@ srcDir        = "src"
 # Dependencies
 
 requires "nim >= 0.17.3"
+
+# Configuration
+
+proc testDebugConfig() =
+  --define: debug
+  --path: "../src"
+  --run
+
+proc testReleaseConfig() =
+  --define: release
+  --path: "../src"
+  --run
+
+# Tasks
+
+task test, "run objc tests":
+  testDebugConfig()
+  setCommand "c", "tests/tall.nim"
+
+task testRelease, "run objc tests in release mode":
+  testReleaseConfig()
+  setCommand "c", "tests/tall.nim"
---
+++ tests/tclassMacro.nim
@@ -0,0 +1,76 @@
+import objc.base, objc.hli, objc.macroCommons, objc.classMacro
+import unittest
+
+importClass NSObject
+objectiveClass RootTest of NSObject
+objectiveClass InheritTest of RootTest
+objectiveClass RootFieldsTest of NSObject:
+  a: int
+  b: cstring
+objectiveClass InheritNoFieldsTest of RootFieldsTest
+objectiveClass InheritFieldsTest of RootFieldsTest:
+  c: float
+
+{. push hint[XDeclaredButNotUsed]:off .}
+
+suite "class macro":
+  setup:
+    let
+      nso = newNSObject()
+      rt = newRootTest()
+      it = newInheritTest()
+      rft = newRootFieldsTest()
+      inft = newInheritNoFieldsTest()
+      ift = newInheritFieldsTest()
+  test "class allocation":
+    check pointer(nso.id) != nil
+    check pointer(rt.id) != nil
+    check pointer(it.id) != nil
+    check pointer(rft.id) != nil
+    check pointer(inft.id) != nil
+    check pointer(ift.id) != nil
+  test "class variables":
+    check nso.class == class"NSObject"
+    check rt.class == class"RootTest"
+    check rt.super == class"NSObject"
+    check it.class == class"InheritTest"
+    check it.super == class"RootTest"
+    check it.super.super == class"NSObject"
+    check rft.class == class"RootFieldsTest"
+    check rft.super == class"NSObject"
+    check inft.class == class"InheritNoFieldsTest"
+    check inft.super == class"RootFieldsTest"
+    check inft.super.super == class"NSObject"
+    check ift.class == class"InheritFieldsTest"
+  test "class types":
+    doAssert nso.classType is NSObjectClass
+    doAssert rt.classType is RootTestClass
+    doAssert rt.metaClassType is RootTestMetaClass
+    doAssert rt.superClassType is NSObjectClass
+    doAssert nso.type.classType is NSObjectClass
+    doAssert rt.type.classType is RootTestClass
+    doAssert rt.type.metaClassType is RootTestMetaClass
+    doAssert rt.type.superClassType is NSObjectClass
+  test "member fields":
+    let
+      nsoVars = nso.class.ivars
+      rtVars = rt.class.ivars
+      itVars = it.class.ivars
+      rftVars = rft.class.ivars
+      iftVars = ift.class.ivars
+      inftVars = inft.class.ivars
+    check nsoVars.len == 1
+    check nsoVars[0].name == "isa"
+    check nsoVars[0].typeEncoding == "#"
+    check rtVars.len == 0
+    check itVars.len == 0
+    check rftVars.len == 2
+    check rftVars[0].name == "a"
+    check rftVars[1].name == "b"
+    check rftVars[0].typeEncoding == "q"
+    check rftVars[1].typeEncoding == "*"
+    check iftVars.len == 1
+    check iftVars[0].name == "c"
+    check iftVars[0].typeEncoding == "d"
+
+{. pop .}
